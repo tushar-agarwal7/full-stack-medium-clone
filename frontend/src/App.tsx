88da6e68d5597a0d105cedccb8d5a55fcb52e0fc
tushar-agarwal7 <tushar.agarwal7373@gmail.com>
2024-03-26 02:10:55 +0530

    add some fe

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import { Blog } from './pages/Blog'
 import { Blogs } from './pages/Blogs'
 import { Publish } from './pages/Publish'
 import { Home } from './pages/Home'
-import { NotFound } from './pages/NotFound'
+
 
 function App() {
 const login=window.localStorage.getItem("token")
@@ -16,10 +16,10 @@ const login=window.localStorage.getItem("token")
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
-          <Route path={"/blogs"} element={ login?<Blogs />:<Home/>} />
-          <Route path={login?"/publish":"/"} element={login?<Publish/>:<Home />} />
+          <Route path="/blogs" element={ <Blogs />} />
+          <Route path="/publish" element={login?<Publish/>:<Home />} />
           <Route path="/" element={<Home/>} />
-          <Route path="*" element={<NotFound />} />
+         
         </Routes>
       </BrowserRouter>
     </>
